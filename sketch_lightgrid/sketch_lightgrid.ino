b2e29fbff5627869e7d824e7a7444e7422e8836b
Josh Fox <jjv360@me.com>
2022-11-11 20:09:01 +0200

    Smooth christmas lights

--- sketch_lightgrid/sketch_lightgrid.ino
+++ sketch_lightgrid/sketch_lightgrid.ino
@@ -18,15 +18,44 @@ void setup() {
 }
 
 int counter = 0;
+float fadeAmt = 0.0;
 void loop() {
 
-    // Set all off
-    for (int i = 0 ; i < LED_COUNT ; i++) leds[i] = CRGB::Black;
+  // Adjust fade
+  float time = millis();
+  float fadeAmt = (1 + sin(time / 1000.0)) / 2;
 
-    // Set one LED on
-    counter++;
-    if (counter >= LED_COUNT) counter = 0;
-    leds[counter] = counter % 2 ? CRGB::Red : CRGB::Green;
+  // Position
+  int drift = 5;
+  float pos = fadeAmt * LED_COUNT;
+
+  // Set all
+  for (int i = 0 ; i < LED_COUNT ; i++) {
+
+    // Set color
+    leds[i].r = 255;
+    leds[i].g = 0;
+    leds[i].b = 0;
+    if (i % 2 == 0) {
+      leds[i].g = leds[i].r;
+      leds[i].r = 0;
+    }
+
+    // Opacity based on position
+    float opacity = max(0, min(1, 1 - abs(i - pos) / drift));
+    leds[i].r *= opacity;
+    leds[i].g *= opacity;
+    leds[i].b *= opacity;
+
+  }
+
+    // Set all off
+    // for (int i = 0 ; i < LED_COUNT ; i++) leds[i] = CRGB::Black;
+
+    // // Set one LED on
+    // counter++;
+    // if (counter >= LED_COUNT) counter = 0;
+    // leds[counter] = counter % 2 ? CRGB::Red : CRGB::Green;
 
     // Show it
     FastLED.show();
